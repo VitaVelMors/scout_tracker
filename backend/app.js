d56e57980de0e253fb76e850b3d49fb909df477a
VitaVelMors <bwhitemedic@icloud.com>
2022-11-14 15:37:35 -0600

    new update

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const { Pool } = require('pg');
 const config = require('./config.js')[process.env.NODE_ENV]
+const cors = require('cors');
+const corsOptions={
+  origin: 'https://scout-tracker.onrender.com/',
+  optionSuccessStatus: 200
+};
 
 const pool = new Pool({connectionString:config.connectionString});
 const app = express();
 const port = 3000;
 
-
+app.options('*', corsOptions);
+app.use(corsOptions);
 app.use(express.json());
 
 app.get('/', (req, res) =>{
   res.send('Hello World!');
 });
 
-app.get('/api/scouts', (req, res) => {
+app.get('/api/scouts', cors(corsOptions), (req, res) => {
   console.log('fun times')
   pool.query('SELECT * FROM scouts')
     .then(result => {
@@ -23,7 +29,7 @@ app.get('/api/scouts', (req, res) => {
     .catch(e => console.error(e.stack))
 });
 
-app.get('/api/scouts/:scout_id', (req, res) => {
+app.get('/api/scouts/:scout_id', cors(corsOptions), (req, res) => {
   console.log(req.params.id)
   async function getScout(){
     try{
@@ -41,7 +47,7 @@ app.get('/api/scouts/:scout_id', (req, res) => {
   getScout()
 });
 
-app.post('/api/scouts', (req, res) => {
+app.post('/api/scouts', cors(corsOptions), (req, res) => {
   let scout = req.body;
   let name = scout.name;
   let age = scout.age;
@@ -62,7 +68,7 @@ app.post('/api/scouts', (req, res) => {
   postScout()
 });
 
-app.patch('/api/scouts/:scout_id', (req,res) => {
+app.patch('/api/scouts/:scout_id', cors(corsOptions), (req,res) => {
   let scout = req.body;
   let name = scout.name;
   let age = scout.age;
@@ -81,7 +87,7 @@ app.patch('/api/scouts/:scout_id', (req,res) => {
   patchFood()
 })
 
-app.delete('/scout/:scout_id', (req,res) => {
+app.delete('/scout/:scout_id', cors(corsOptions), (req,res) => {
   console.log(req.params.id)
   async function deleteFood(){
     try{
