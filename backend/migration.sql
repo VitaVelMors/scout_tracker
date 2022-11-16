6d90439ab94d9154df2e8048dcff438af1e24625
VitaVelMors <bwhitemedic@icloud.com>
2022-11-15 23:55:58 -0600

    update

--- backend/migration.sql
+++ backend/migration.sql
@@ -16,21 +16,21 @@ CREATE TABLE achievements (
 );
 
 INSERT INTO scouts (name, age, image)
-VALUES ('Anakin Skywalker', 9, 'images\anakin.png' ),
-  ('Obi-Wan Kenobi', 10, 'images\obi-wan-kenobi.png'),
-  ('Depa Billaba', 10, 'images\depa-billaba.png'),
-  ('Kit Fisto', 9, 'images\Kit-Fisto.png'),
-  ('Adi Gallia', 9, 'images\Adi-Gallia.png'),
-  ('Qui-Gon Jinn', 10, 'images\qui-gon-jinn.png'),
-  ('Mace Windu', 9, 'images\Mace-Windu.png'),
-  ('Plo Koon', 10, 'images\plo-koon.png'),
-  ('Luminara Unduli', 9, 'images\luminara-undulli.png'),
-  ('Even Piell', 9, 'images\Even-Piell.png'),
-  ('Yarael Poof', 10, 'images\Yarael-Poof.png'),
-  ('Oppo Rancisis', 10, 'images\oppo-rancisis.png'),
-  ('Shaak Ti', 9, 'images\shaak-ti.png'),
-  ('Quinlan Vos', 9, 'images\quinlan-vos.png'),
-  ('Barriss Offee', 9, 'images\Barriss-Offee.png');
+VALUES ('Anakin Skywalker', 9, 'images/anakin.png' ),
+  ('Obi-Wan Kenobi', 10, 'images/obi-wan-kenobi.png'),
+  ('Depa Billaba', 10, 'images/depa-billaba.png'),
+  ('Kit Fisto', 9, 'images/Kit-Fisto.png'),
+  ('Adi Gallia', 9, 'images/Adi-Gallia.png'),
+  ('Qui-Gon Jinn', 10, 'images/qui-gon-jinn.png'),
+  ('Mace Windu', 9, 'images/Mace-Windu.png'),
+  ('Plo Koon', 10, 'images/plo-koon.png'),
+  ('Luminara Unduli', 9, 'images/luminara-undulli.png'),
+  ('Even Piell', 9, 'images/Even-Piell.png'),
+  ('Yarael Poof', 10, 'images/Yarael-Poof.png'),
+  ('Oppo Rancisis', 10, 'images/oppo-rancisis.png'),
+  ('Shaak Ti', 9, 'images/shaak-ti.png'),
+  ('Quinlan Vos', 9, 'images/quinlan-vos.png'),
+  ('Barriss Offee', 9, 'images/Barriss-Offee.png');
 
   INSERT INTO achievements (ach_name)
   VALUES ('Webelos Walkabout'),
